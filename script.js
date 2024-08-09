9fef4ebdb909ef4f6d1772cfc49be29e4913d545
Mapucheco <159633354+Elmapucheco@users.noreply.github.com>
2024-08-09 10:55:01 +0200

    adjusting project

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const projectOverlay = document.querySelector(".project-overlay");
+
+  if (projectOverlay) {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            // Añadir la clase de animación cuando el overlay entra en el viewport
+            projectOverlay.classList.add("animate");
+          } else {
+            // Eliminar la clase de animación cuando el overlay sale del viewport
+            projectOverlay.classList.remove("animate");
+          }
+        });
+      },
+      {
+        threshold: [0], // Ajusta el umbral si es necesario
+      }
+    );
+
+    observer.observe(projectOverlay);
+  }
+});
